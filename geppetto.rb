8df3412c801fd9afe074d2595c1fa3cc8cd00ec4
Solomon Kimrey <89326872+skimrey@users.noreply.github.com>
2024-02-03 17:04:47 -0600

    Update geppetto.rb

--- geppetto.rb
+++ geppetto.rb
@@ -2,24 +2,47 @@ require 'net/http'
 require 'uri'
 require 'action_view'
 
-include ActionView::Helpers::SanitizeHelper
-puts "Hi, I'm Brainbot. Pleased to conquer you. Type exit at any time to leave the chat."
-x = ""
-while x != "exit\n"
-  prompt = gets
-  message = URI.encode_www_form_component(prompt)
-  url = URI.parse("https://botlibre.com/rest/api/form-chat?instance=46893897&message=#{message}&application=6410829901268491461")
-  req = Net::HTTP::Get.new(url.to_s)
-  res = Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == 'https') {|http|
-    http.request(req)
-}
+class BrainBot
+  include ActionView::Helpers::SanitizeHelper
 
-# Get plain text response and remove HTML-like elements (buttons)
+  attr_accessor :exit_flag
+
+  def initialize
+    @exit_flag = false
+  end
+
+  def start_chat
+    puts "Hi, I'm Brainbot. Pleased to conquer you. Type 'exit' at any time to leave the chat."
+
+    while !@exit_flag
+      prompt_user
+      get_bot_response
+    end
+  end
+
   
-  plain_text_response = strip_tags(res.body)
-  plain_text_response = plain_text_response.gsub("&lt;button&gt;", '')
-  plain_text_response = plain_text_response.gsub("&lt;/button&gt;", '')
-  plain_text_response = plain_text_response.gsub("Brain Bot: ", '')
-  puts "#{plain_text_response}"
-  x = prompt
+
+  def prompt_user
+    prompt = gets.chomp
+    @exit_flag = true if prompt.downcase == 'exit'
+    @user_input = URI.encode_www_form_component(prompt)
+  end
+
+  def get_bot_response
+    url = URI.parse("https://botlibre.com/rest/api/form-chat?instance=46893897&message=#{@user_input}&application=6410829901268491461")
+    req = Net::HTTP::Get.new(url.to_s)
+    res = Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == 'https') {|http|
+      http.request(req)
+    }
+
+    plain_text_response = strip_tags(res.body)
+    plain_text_response = plain_text_response.gsub("&lt;button&gt;", '')
+    plain_text_response = plain_text_response.gsub("&lt;/button&gt;", '')
+    plain_text_response = plain_text_response.gsub("Brain Bot: ", '')
+
+    puts "#{plain_text_response}"
+  end
 end
+
+brain_bot = BrainBot.new
+brain_bot.start_chat
